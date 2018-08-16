f128d7c5168976e83dc65c6b4f58aaf2097d8b1f
Katie Baltzley <kmbaltzl@gmail.com>
2018-08-16 12:46:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Phil Ochs']= 'Here\'s to the State of Mississippi'
 }
 
-function removeFromPlaylist(playlist, artistName, songTitle) {
+function removeFromPlaylist(playlist, artistName) {
   delete playlist.alison
 }
