7498f06530126c7919be88458c56d1ead9883458
Katie Baltzley <kmbaltzl@gmail.com>
2018-08-16 12:22:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.'Phil Ochs'= 'Here\'s to 
+  playlist.'Phil Ochs'= "Here\'s to the State of Mississippi"
 }
